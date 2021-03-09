97bbeee34aa5ac8dd4a381cd882cca4251f2e904
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2021-03-09 22:57:05 +0100

    Adding command line options

--- change_mixer.py
+++ change_mixer.py
@@ -1,53 +1,102 @@
 import lxml.etree as etree
 
-do_volume = True
-do_boost = False
-do_mic1 = True
-do_mic2 = True
-AMPL = 5
-BOOST = 1
-
 filein = './mixer_paths_0.xml'
 fileout = './mixer_paths_0_python.xml'
 
-def volume(val_str):    
+
+def volume(val_str, mic_gain):
     val = int(val_str)
-    return f'{val*AMPL}'
+    return f'{val*mic_gain}'
 
-def boost(val_str):
+
+def boost(val_str, mic_boost):
     val = int(val_str)
-    return f'{val+BOOST}'
-
-parser = etree.XMLParser(remove_blank_text=False)
-
-root = etree.parse(filein, parser)
-
-# breakpoint()
-
-if do_volume:
-    if do_mic1:
-        for mic in root.xpath("//ctl[@name='MIC1 Volume']"):
-            attr = mic.attrib
-            attr['value'] = volume(attr['value'])
-
-    if do_mic2:
-        for mic in root.xpath("//ctl[@name='MIC2 Volume']"):
-            attr = mic.attrib
-            attr['value'] = volume(attr['value'])
-
-if do_boost:
-    if do_mic1:
-        for mic in root.xpath("//ctl[@name='MIC1 Boost Volume']"):
-            attr = mic.attrib
-            attr['value'] = boost(attr['value'])
-
-    if do_mic2:
-        for mic in root.xpath("//ctl[@name='MIC2 Boost Volume']"):
-            attr = mic.attrib
-            attr['value'] = boost(attr['value'])
+    return f'{val+mic_boost}'
 
 
-# print(etree.tostring(root, pretty_print=True))
+def main(mic1_gain, mic2_gain):
+    do_volume = False
+    do_boost = False
 
-root.write(fileout, encoding="UTF-8", pretty_print=False,
-                strip_text=False, with_tail=True)
+    if mic1_gain != -1:
+        do_mic1 = True
+        do_volume = True
+    else:
+        do_mic1 = False    
+    
+    if mic2_gain != -1:
+        do_mic2 = True
+        do_volume = True
+    else:
+        do_mic2 = False    
+    
+    # breakpoint()
+    parser = etree.XMLParser(remove_blank_text=False)
+
+    root = etree.parse(filein, parser)
+
+    # breakpoint()
+
+    if do_volume:
+        if do_mic1:
+            for mic in root.xpath("//ctl[@name='MIC1 Volume']"):
+                attr = mic.attrib
+                attr['value'] = volume(attr['value'], mic1_gain)
+
+        if do_mic2:
+            for mic in root.xpath("//ctl[@name='MIC2 Volume']"):
+                attr = mic.attrib
+                attr['value'] = volume(attr['value'], mic2_gain)
+
+    if do_boost:
+        if do_mic1:
+            for mic in root.xpath("//ctl[@name='MIC1 Boost Volume']"):
+                attr = mic.attrib
+                attr['value'] = boost(attr['value'], 1)
+
+        if do_mic2:
+            for mic in root.xpath("//ctl[@name='MIC2 Boost Volume']"):
+                attr = mic.attrib
+                attr['value'] = boost(attr['value'], 1)
+
+
+    # print(etree.tostring(root, pretty_print=True))
+
+    root.write(fileout, encoding="UTF-8", pretty_print=False,
+            strip_text=False, with_tail=True)
+
+
+if __name__ == "__main__":
+    import argparse
+    # from six import text_type
+
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument(
+        '-m', '--mic1_gain',
+        dest='mic1_gain',
+        type=int,
+        default=-1,
+        help='specifies the gain for mic 1',
+    )
+    parser.add_argument(
+        '-M', '--mic2_gain',
+        dest='mic2_gain',
+        type=int,
+        default=-1,
+        help='specifies the gain for mic 2',
+    )
+
+    args, unknown = parser.parse_known_args()
+
+    try:
+        main(
+            args.mic1_gain,
+            args.mic2_gain,
+        )
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("Done\n")
--- convert.sh
+++ convert.sh
@@ -1,17 +1,87 @@
 #!/bin/bash
 
+is_dryrun=n
+do_mic1=n
+do_mic2=n
+
+while getopts "a:b:dt" options
+do
+    case "${options}" in
+        a)
+            re_isanum='^[0-9]+$'
+            if ! [[ ${OPTARG} =~ $re_isanum ]]
+            then
+                echo "Error: gain must be a positive, whole number."
+                exit 1
+            fi
+            mic1_gain=${OPTARG}
+            do_mic1=y
+        ;;
+        
+        b)
+            re_isanum='^[0-9]+$'
+            if ! [[ ${OPTARG} =~ $re_isanum ]]
+            then
+                echo "Error: gain must be a positive, whole number."
+                exit 1
+            fi
+            mic2_gain=${OPTARG}
+            do_mic2=y
+        ;;
+        d)
+            do_diff=y
+        ;;
+        t)
+            is_dryrun=y
+        ;;
+        :)
+            echo "Error: -${OPTARG} requires an argument."
+            exit 1
+        ;;
+        *)
+            exit 1
+        ;;
+    esac
+done
+
 # python --version
 # Python 3.8.0
-python change_mixer.py
+params=""
+if [ "${do_mic1} " == 'y ' ]
+then
+    params="${params} --mic1_gain ${mic1_gain}"
+fi
+
+if [ "${do_mic2} " == 'y ' ]
+then
+    params="${params} --mic2_gain ${mic2_gain}"
+fi
+
+python change_mixer.py ${params}
+
+orig_file=mixer_paths_0.xml
+py_file=mixer_paths_0_python.xml
+proc_file=mixer_paths_0_proc.xml
 
 # The following is because the output has "value"/> instead of "value" />
 # XML-wise it is equivalent but it hampers the diffing of the files
-cat mixer_paths_0_python.xml | sed 's/"\/>$/" \/>/g' > mixer_paths_0_proc.xml
+cat ${py_file} | sed 's/"\/>$/" \/>/g' > ${proc_file}
+
+if [ "${do_diff} " == 'y ' ]
+then
+    diff ${orig_file} ${proc_file} | more
+fi
 
 # adb --version
 # Android Debug Bridge version 1.0.41
 # Version 30.0.5-6877874
-adb root
-adb remount
-adb push ./mixer_paths_0_proc.xml /system/vendor/etc/mixer_paths_0.xml
-adb reboot
+if [ "${is_dryrun} " != 'y ' ]
+then
+    echo "Pushing file to phone"
+    adb root
+    adb remount
+    adb push ./mixer_paths_0_proc.xml /system/vendor/etc/mixer_paths_0.xml
+    adb reboot
+else
+    echo "Not pushing file to phone"
+fi
